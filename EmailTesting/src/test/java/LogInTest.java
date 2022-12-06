0f607627d88325e29f66bccd136bbd358095e30b
Kirill Babich <gonertheowner@gmail.com>
2022-12-06 17:21:22 +0300

    Created LogInTest template and updated chromedriver.exe

---
+++ EmailTesting/src/test/java/LogInTest.java
@@ -0,0 +1,37 @@
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import pages.LogInPage;
+
+import java.util.concurrent.TimeUnit;
+public class LogInTest {
+    public static LogInPage loginPage;
+    public static WebDriver driver;
+
+    @BeforeClass
+    public static void setup() {
+        //определение пути до драйвера и его настройка
+        System.setProperty("webdriver.chrome.driver", ConfProperties.getProperty("chromedriver"));
+
+        //создание экземпляра драйвера
+        driver = new ChromeDriver();
+
+        loginPage = new LogInPage(driver);
+
+        //окно разворачивается на полный экран
+        driver.manage().window().maximize();
+
+        //задержка на выполнение теста = 10 сек.
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        //получение ссылки на страницу из файла настроек
+        driver.get(ConfProperties.getProperty("page"));
+    }
+
+    @Test
+    public void logInTest() {
+        loginPage.inputEmail(ConfProperties.getProperty("email1"));
+    }
+}
